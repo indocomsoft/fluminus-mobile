2fcdeeb60dc6b63c084035363847de844b517d3f
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-12-24 14:35:37 +0700

    Add SwiftLint and SwiftFormat

--- ios/Fluminus/FluminusTests/FluminusTests.swift
+++ ios/Fluminus/FluminusTests/FluminusTests.swift
@@ -6,11 +6,10 @@
 //  Copyright © 2019 Julius. All rights reserved.
 //
 
-import XCTest
 @testable import Fluminus
+import XCTest
 
 class FluminusTests: XCTestCase {
-
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -26,9 +25,8 @@ class FluminusTests: XCTestCase {
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
