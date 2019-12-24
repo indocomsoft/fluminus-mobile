836ab554f4081185ca6673406697f20138105f79
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-12-24 13:08:00 +0700

    Add SwiftUI base

---
+++ ios/Fluminus/Fluminus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fluminus
+//
+//  Created by Julius on 24/12/19.
+//  Copyright © 2019 Julius. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
