5c3aab4621d3ec8d562b29fb1524f37650ddbbe4
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-12-25 16:35:48 +0700

    Add Account model

---
+++ ios/Fluminus/Fluminus/Models/Account.swift
@@ -0,0 +1,14 @@
+//
+//  Account.swift
+//  Fluminus
+//
+//  Created by Julius on 25/12/19.
+//  Copyright © 2019 Julius. All rights reserved.
+//
+
+import Combine
+import Foundation
+
+class Account: ObservableObject {
+    
+}
